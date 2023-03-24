9a9600d3283a0b7e21d9b81e271ad609f99440f7
Dan Patton <patton.dan@gmail.com>
2023-03-24 21:43:38 +0000

    Bit twiddling

--- Chessica.Core/BitBoard.cs
+++ Chessica.Core/BitBoard.cs
@@ -32,15 +32,29 @@ public struct BitBoard : IEnumerable<Coord>
         _state &= ~(1ul << coord.Ordinal);
     }
 
+    private const ulong DeBruijnSequence = 0x37E84A99DAE458F;
+
+    private static readonly int[] MultiplyDeBruijnBitPosition =
+    {
+        0, 1, 17, 2, 18, 50, 3, 57,
+        47, 19, 22, 51, 29, 4, 33, 58,
+        15, 48, 20, 27, 25, 23, 52, 41,
+        54, 30, 38, 5, 43, 34, 59, 8,
+        63, 16, 49, 56, 46, 21, 28, 32,
+        14, 26, 24, 40, 53, 37, 42, 7,
+        62, 55, 45, 31, 13, 39, 36, 6,
+        61, 44, 12, 35, 60, 11, 10, 9,
+    };
+
     public IEnumerator<Coord> GetEnumerator()
     {
-        for (byte i = 0x00; i < 0x40; ++i)
+        var bb = _state;
+        while (bb != 0)
         {
-            var bit = 1ul << i;
-            if ((_state & bit) == bit)
-            {
-                yield return Coord.FromOrdinal(i);
-            }
+            var ordinal = MultiplyDeBruijnBitPosition[((ulong)((long)bb & -(long)bb) * DeBruijnSequence) >> 58];
+            var coord = Coord.FromOrdinal(ordinal);
+            yield return coord;
+            bb &= ~(1ul << ordinal);
         }
     }
 
