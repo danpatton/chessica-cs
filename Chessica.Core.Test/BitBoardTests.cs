239d6e3e4b60ab1bcd4fcaaf3c72f40284a053c1
Dan Patton <patton.dan@gmail.com>
2023-03-25 08:22:35 +0000

    Add BitBoard test

---
+++ Chessica.Core.Test/BitBoardTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Chessica.Core.Test;
+
+[TestFixture]
+public class BitBoardTests
+{
+    [Test]
+    public void TestEnumeration()
+    {
+        BitBoard bb = 0;
+        Coord a4 = "a4";
+        Coord b5 = "b5";
+        bb |= a4;
+        bb |= b5;
+        var coords = bb.ToArray();
+        Assert.That(coords, Has.Length.EqualTo(2));
+        Assert.That(coords, Does.Contain(a4));
+        Assert.That(coords, Does.Contain(b5));
+    }
+}
